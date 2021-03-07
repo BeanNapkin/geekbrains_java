2e62dcae9d5593cee5ab7d14a7a6a6b62bdd49ac
MargaritaFateeva <margarita@fateev.pro>
2021-03-08 02:02:47 +0300

    Homework.Threads. Race

--- Homework/src/HomeworkThreads/MainClass.java
+++ Homework/src/HomeworkThreads/MainClass.java
@@ -1,35 +1,31 @@
 package HomeworkThreads;
 
+import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
 
-public class Test {
-    public static void main(String[] args) {
-        // задаем количество потоков
-        final int THREADS_COUNT = 6;
-        // задаем значение счетчика
-        final CountDownLatch cdl = new CountDownLatch(THREADS_COUNT);
-        System.out.println("Начинаем");
-        for (int i = 0; i < THREADS_COUNT; i++) {
-            final int w = i;
-            new Thread(() -> {
-                try {
-                    // считаем, что выполнение задачи занимает ~1 сек
-                    Thread.sleep(500 + (int)(500 * Math.random()));
-                    cdl.countDown();
-                    // как только задача выполнена, уменьшаем счетчик
-                    System.out.println("Поток #" + w + " - готов");
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }).start();
+public class MainClass {
+    public static final int CARS_COUNT = 4;
+
+    public static void main(String[] args)  {
+
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
+
+        Race race = new Race(CARS_COUNT, new Road(60), new Tunnel(), new Road(40));
+        Car[] cars = new Car[CARS_COUNT];
+
+        for (int i = 0; i < cars.length; i++) {
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 100));
         }
-        try {
-            cdl.await();   // пока счетчик не приравняется нулю, будем стоять на этой строке
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+
+        for (int i = 0; i < cars.length; i++) {
+            new Thread(cars[i]).start();
         }
-// как только все потоки выполнили свои задачи - пишем сообщение
-        System.out.println("Работа завершена");
+
+        race.awaitPreparationDone();
+
+        race.awaitRaceDone();
+
+        System.out.println("Победитель: " + race.getWinner().getName());
     }
-
 }
