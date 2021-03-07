53649174e28bdf9a8ed0be5668bf43fdf7e8439c
Margarita Fateeva <margarita@fateev.pro>
2021-03-08 02:04:36 +0300

    Homework 13. Threads. Race. Update

--- Homework/src/HomeworkThreads/Race.java
+++ Homework/src/HomeworkThreads/Race.java
@@ -1,2 +1,78 @@
-package HomeworkThreads;public class Race {
+package HomeworkThreads;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+public class Race {
+    private ArrayList<Stage> stages;
+
+    public ArrayList<Stage> getStages() {
+        return stages;
+    }
+
+    private Car winner;
+
+    private CyclicBarrier preparationBarrier;
+    private CyclicBarrier finishBarrier;
+
+    public Car getWinner() {
+        return winner;
+    }
+
+    public Race(int carsCount, Stage... stages) {
+        preparationBarrier = new CyclicBarrier(carsCount + 1);
+        finishBarrier = new CyclicBarrier(carsCount + 1);
+        this.stages = new ArrayList<>(Arrays.asList(stages));
+    }
+
+    public void preparationDone() {
+        try {
+            preparationBarrier.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (BrokenBarrierException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void raceDone(Car car) {
+
+        if (winner == null) {
+            winner = car;
+        }
+
+        try {
+            System.out.println(car.getName() + " закончил гонку.");
+            finishBarrier.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (BrokenBarrierException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void awaitPreparationDone() {
+        try {
+            preparationBarrier.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (BrokenBarrierException e) {
+            e.printStackTrace();
+        }
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+    }
+
+    public void awaitRaceDone() {
+        try {
+            finishBarrier.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (BrokenBarrierException e) {
+            e.printStackTrace();
+        }
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+    }
+
 }
--- Homework/src/HomeworkThreads/Stage.java
+++ Homework/src/HomeworkThreads/Stage.java
@@ -1,2 +1,12 @@
-package HomeworkThreads;public class Stage {
+package HomeworkThreads;
+
+public abstract class Stage {
+    protected int length;
+    protected String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public abstract void go(Car c);
 }
